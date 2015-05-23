e51fc4c886b76b3b6965689973bf8f739ad18130
ulfw <ulf.worsoe@mosek.com>
2015-05-23 17:24:08 +0200

    build.rs script

---
+++ build.rs
@@ -0,0 +1,4 @@
+fn main() {
+    println!("cargo:rustc-link-search=[\"/home/ulfw/mosekprj/git/dev/bld/karise/final/default/minidist/bin\"]");
+    println!("cargo:rustc-flags=-L /home/ulfw/mosekprj/git/dev/bld/karise/final/default/minidist/bin");
+}
