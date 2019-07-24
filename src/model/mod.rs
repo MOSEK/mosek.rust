d88d5e2dd3f0a2ec4a0b8ac1f0b3d46ddfa8f533
ulfw <ulf.worsoe@mosek.com>
2019-07-24 08:35:01 +0200

    playing with model

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -5,9 +5,65 @@ pub struct Model {
     numpsdatoms : i64,
 }
 
+
+
+pub trait IndexGenerator {
+    fn next(& mut self, &mut String);
+    fn skip(& mut self);
+}
+
+pub type FlatIndexer = u64;
+
+impl IndexGenerator for FlatIndexer {
+    fn next(& mut self, b : & mut String) {
+        if *self == 0 {
+            b.push('0')
+        }
+        else {
+            let mut n = 1;
+            while n*10 < *self { n *= 10; }
+            let v = *self;
+            while n > 0 {
+                let c = (v / n) % 10 + ('0' as u64);
+                b.push((c as u8) as char);
+                n /= 10;
+            }
+        }
+        self.skip();
+    }
+    fn skip(& mut self) { (*self) += 1; }
+}
+
+
+pub trait NameGenerator {
+    fn next(& mut self, &mut String);
+    fn skip(& mut self);
+}
+
+pub struct FlatNamer<IdxGen:IndexGenerator> {
+    name : String,
+    idx  : IdxGen
+}
+
+impl<IdxGen:IndexGenerator> FlatNamer<IdxGen> {
+    fn new(name : &str, idxgen : IdxGen) -> FlatNamer<IdxGen> { return FlatNamer{ name : String::from(name), idx : idxgen} }
+}
+
+impl<IdxGen:IndexGenerator> NameGenerator for FlatNamer<IdxGen> {
+    fn skip(&mut self) { self.idx.skip(); }
+    fn next(&mut self, buf : & mut String) {
+        buf.clear();
+        buf.push_str(&self.name);
+        buf.push('[');
+        self.idx.next(buf);
+        buf.push(']');
+    }
+}
+
 pub struct Variable {
     idxs : Vec<i64>,
 }
+
 impl Variable {
     pub fn size(&self) -> usize { return self.idxs.len() }
 }
@@ -21,6 +77,7 @@ pub struct LinearBound {
     rhs : Vec<f64>,
     bk  : u8,
 }
+
 impl LinearBound {
     pub fn size(&self) -> usize { return self.rhs.len() }
 }
@@ -38,11 +95,12 @@ impl Domain for LinearBound {
 
         let idxs32 = m.alloc_vars(self.size() as i32, bk, self.rhs.as_slice());
         let idxs64 = idxs32.iter().map(|x| *x as i64).collect();
+
+
+
         return Variable{ idxs:idxs64 };
     }
 }
-
-
 
 impl Model {
     pub fn new() -> Model {
@@ -54,7 +112,7 @@ impl Model {
         task.append_vars(1);
         task.put_var_name(0,"1.0");
 
-        return Model{ env, task, slack, numpsdatoms };
+        return Model{ env:env, task:task, slack:slack, numpsdatoms:numpsdatoms };
     }
 
     fn alloc_vars(&self, n : i32, bk : i32, b : &[f64]) -> Vec<i32> {
@@ -77,9 +135,8 @@ impl Model {
         return numcone;
     }
 
-    pub fn variable<Dom:Domain>(& mut self,name : &str, dom : &Dom) -> Variable {
+    pub fn variable<Dom:Domain>(& mut self, dom : &Dom) -> Variable {
         let v =  dom.alloc_var_block(self);
-
         return v;
     }
 
