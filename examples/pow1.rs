6e5679402d5db2844cd8cca509fd0d5ef03be9b1
ulfw <ulf.worsoe@mosek.com>
2022-07-06 14:13:54 +0000

    update example

--- examples/pow1.rs
+++ examples/pow1.rs
@@ -11,31 +11,26 @@
 extern crate mosek;
 use mosek::*;
 
-const INFTY : f64 = 0.0;
+const INF : f64 = 0.0;
 
 fn main()  -> Result<(),String> {
     let numcon : i32 = 1;
-    let numvar : i32 = 6;
+    let numvar : i32 = 5;
 
     // Since the value infinity is never used, we define
     // 'infinity' symbolic purposes only
 
-    let val = vec![ 1.0, 1.0, -1.0 ];
-    let sub = vec![ 3,   4,    0 ];
+    let cval = vec![ 1.0, 1.0, -1.0 ];
+    let csub = vec![ 3,   4,    0 ];
 
     let aval = vec![ 1.0, 1.0, 0.5 ];
     let asub = vec![ 0, 1, 2 ];
 
-    /* Create the mosek environment. */
-    let env = match mosek::Env::new() {
-        Some(e) => e,
-        None => return Err("Failed to create env".to_string()),
-        };
     /* Create the optimization task. */
-    let mut task = match env.task() {
+    let mut task = match Task::new() {
         Some(e) => e,
         None => return Err("Failed to create task".to_string()),
-        };
+    };
     // Directs the log task stream to the user specified
     // method msgclass.streamCB
     task.put_stream_callback(Streamtype::LOG, |msg| print!("{}",msg))?;
@@ -50,57 +45,55 @@ fn main()  -> Result<(),String> {
     task.append_vars(numvar)?;
 
     /* Set up the linear part of the problem */
-    task.put_c_list(&sub, &val)?;
+    task.put_c_list(&csub, &cval)?;
     task.put_a_row(0, &asub, &aval)?;
     task.put_con_bound(0, Boundkey::FX, 2.0, 2.0)?;
 
 
-    let bkx = vec![Boundkey::FR, Boundkey::FR, Boundkey::FR, Boundkey::FR, Boundkey::FR, Boundkey::FX];
-    let blx = vec![-INFTY,    -INFTY,    -INFTY,    -INFTY,    -INFTY,    1.0      ];
-    let bux = vec![ INFTY,     INFTY,     INFTY,     INFTY,     INFTY,    1.0      ];
-    task.put_var_bound_slice(0, numvar, &bkx, &blx, &bux)?;
+    task.put_var_bound_slice_const(0, numvar, Boundkey::FR, -INF, INF)?;
 
     /* Add a conic constraint */
-    task.append_cone(Conetype::PPOW, 0.2, &vec![0, 1, 3])?;
-    task.append_cone(Conetype::PPOW, 0.4, &vec![2, 5, 4])?;
+    let pc1 = task.append_primal_power_cone_domain(3, &[0.2, 0.8])?;
+    let pc2 = task.append_primal_power_cone_domain(3, &[4.0, 6.0])?;
+
+    // Create data structures F,g so that
+    //
+    //   F * x + g = (x(0), x(1), x(3), x(2), 1.0, x(4))
+    //
+    task.append_afes(6)?;
+    task.put_afe_f_entry_list(&[0, 1, 2, 3, 5],         // Rows
+                              &[0, 1, 3, 2, 4],         // Columns
+                              &[1.0, 1.0, 1.0, 1.0, 1.0])?;
+    task.put_afe_g(4, 1.0)?;
+
+    // Append the two conic constraints
+    task.append_acc(pc1,                  // Domain
+                    &[0, 1, 2],           // Rows from F
+                    &[0.0,0.0,0.0])?;     // Unused
+    task.append_acc(pc2,                  // Domain
+                    &[3, 4, 5],           // Rows from F
+                    &[0.0,0.0,0.0])?;     // Unused
     //TAG:end-appendcone
 
     task.put_obj_sense(Objsense::MAXIMIZE)?;
     task.optimize()?;
 
+    task.write_data("pow1.ptf")?;
     // Print a summary containing information
     // about the solution for debugging purposes
     task.solution_summary(Streamtype::LOG)?;
     /* Get status information about the solution */
     let solsta = task.get_sol_sta(Soltype::ITR)?;
 
-    match solsta {
-        Solsta::OPTIMAL => {
-            let mut xx = vec![0.0; numvar as usize];
-            task.get_xx(Soltype::ITR,    /* Request the basic solution. */
-                        & mut xx[..])?;
+    assert!(solsta == Solsta::OPTIMAL);
 
-            println!("Optimal primal solution");
-            for j in 0..numvar as usize
-            {
-                println!("x[{}]: {}",j,xx[j]);
-            }
-        }
-        Solsta::DUAL_INFEAS_CER |
-        Solsta::PRIM_INFEAS_CER => {
-            println!("Primal or dual infeasibility certificate found.");
-        }
+    let mut xx = vec![0.0; numvar as usize];
+    task.get_xx(Soltype::ITR,
+                xx.as_mut_slice())?;
 
-        Solsta::UNKNOWN => {
-            /* If the solutions status is unknown, print the termination code
-             * indicating why the optimizer terminated prematurely. */
-
-            println!("The solution status is unknown.");
-            println!("The optimizer terminitated with code: {}",solsta);
-          }
-        _ => {
-            println!("Other solution status.");
-        }
+    println!("Optimal primal solution");
+    for (j,&xj) in xx[0..3].iter().enumerate() {
+        println!("x[{}]: {}",j,xj);
     }
 
     return Ok(());
