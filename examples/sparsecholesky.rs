3a2f6ce7b004a4f165ea55225c2aea0c30784dd9
ulfw <ulf.worsoe@mosek.com>
2022-09-16 16:01:56 +0200

    add example

--- examples/sparsecholesky.rs
+++ examples/sparsecholesky.rs
@@ -1,90 +1,121 @@
-#
-#   Copyright: Copyright (c) MOSEK ApS, Denmark. All rights reserved.
-#
-#   File:      sparsecholesky.jl
-#
-#   Purpose:   Demonstrate the sparse Cholesky factorization.
-#
-using Mosek
+/*
+   Copyright: $$copyright
 
-function printsparse(n          :: Int32,
-                     perm       :: Vector{Int32},
-                     diag       :: Vector{Float64},
-                     lnzc       :: Vector{Int32},
-                     lptrc      :: Vector{Int64},
-                     lensubnval :: Int64,
-                     lsubc      :: Vector{Int32},
-                     lvalc      :: Vector{Float64})
-    println("P = $(perm)")
-    println("diag(D) = $(diag)")
+   File:      $${file}
 
-    l = zeros(Int32,(n,n))
-    for j in 1:n
-        for i in lptrc[j]:lptrc[j]+lnzc[j]-1
-            l[lsubc[i],j] = lvalc[i]
-        end
-    end
-    println("L = $l")
-end
+   Purpose:   Demonstrate the sparse Cholesky factorization.
 
-function main()
-    # Example from the manual
-    # Observe that anzc, aptrc, asubc and avalc only specify the lower triangular part.
-    n     = Int32(4)
-    anzc  = Int32[4, 1, 1, 1]
-    asubc = Int32[0, 1, 2, 3,
-                     1,
-                        2,
-                           3]
-    aptrc = Int64[0, 4, 5, 6]
-    avalc = Float64[4.0, 1.0, 1.0, 1.0,
-                         1.0,
-                              1.0,
-                                   1.0]
-    b     = Float64[13.0, 3.0, 4.0, 5.0]
+ */
+package com.mosek.example;
+import mosek.*;
 
-    (perm,
-     diag,
-     lnzc,
-     lptrc,
-     lensubnval,
-     lsubc,
-     lvalc) = computesparsecholesky(Int32(0),  #Mosek chooses number of threads
-                                    Int32(1),  #Apply reordering heuristic
-                                    1.0e-14,  #Singularity tolerance
-                                    anzc, aptrc, asubc, avalc)
-
-    printsparse(n, perm, diag, lnzc, lptrc, lensubnval, lsubc, lvalc)
-
-    # Permuted b is stored as x.
-    x = b[perm]
-
-    # Compute  inv(L)*x.
-    sparsetriangularsolvedense(MSK_TRANSPOSE_NO,  lnzc, lptrc, lsubc, lvalc, x)
-    # Compute  inv(L^T)*x.
-    sparsetriangularsolvedense(MSK_TRANSPOSE_YES, lnzc, lptrc, lsubc, lvalc, x)
-
-    println("\nSolution A x = b, x = $([ x[j] for i in 1..n for j in 1..n if perm[j] == i ])")
-
-    n     = Int32(3)
-    anzc  = Int32[3, 2, 1]
-    asub  = Int32[0, 1, 2, 1, 2, 2]
-    aptr  = Int64[0, 3, 5, ]
-    avalc = Float64[1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
-
-    (perm,
-     diag,
-     lnzc,
-     lptrc,
-     lensubnval,
-     lsubc,
-     lvalc) = computesparsecholesky(0,        #Mosek chooses number of threads
-                                    1,        #Apply reordering heuristic
-                                    1.0e-14,  #Singularity tolerance
-                                    anzc, aptrc, asubc, avalc)
-
-    printsparse(n, perm, diag, lnzc, lptrc, lensubnval, lsubc, lvalc)
-end
+public class sparsecholesky {
+  public static void printsparse(int      n,
+                                 int[]    perm,
+                                 double[] diag,
+                                 int[]    lnzc,
+                                 long[]   lptrc,
+                                 long     lensubnval,
+                                 int[]    lsubc,
+                                 double[] lvalc) {
+    int i, j;
+    System.out.print("P = [ ");
+    for (i = 0; i < n; i++) System.out.print(perm[i] + " ");
+    System.out.println("]");
+    System.out.print("diag(D) = [ ");
+    for (i = 0; i < n; i++) System.out.print(diag[i] + " ");
+    System.out.println("]");
+    double[] l = new double[n * n];
+    for (j = 0; j < n; j++)
+      for (i = (int)lptrc[j]; i < lptrc[j] + lnzc[j]; i++)
+        l[lsubc[i] * n + j] = lvalc[i];
+    System.out.println("L=");
+    for (i = 0; i < n; i++) {
+      for (j = 0; j < n; j++) System.out.print(l[i * n + j] + " ");
+      System.out.println("");
+    }
+  }
 
 
-main()
+  public static void main(String[] args) {
+    /* Create the mosek environment. */
+    try (Env env = new Env()) {
+      {
+        //Example from the manual
+        /*TAG:begin-example1*/
+        //Observe that anzc, aptrc, asubc and avalc only specify the lower triangular part.
+        int n                 = 4;
+        int[] anzc            = {4, 1, 1, 1};
+        int[] asubc           = {0, 1, 2, 3, 1, 2, 3};
+        long[] aptrc          = {0, 4, 5, 6};
+        double[] avalc        = {4.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+        double[] b            = {13.0, 3.0, 4.0, 5.0};
+        /*TAG:end-example1*/
+
+        int[][] perm      = new int[1][];
+        int[][] lnzc      = new int[1][];
+        int[][] lsubc     = new int[1][];
+        long[] lensubnval = new long[1];
+        double[][] diag   = new double[1][];
+        long[][] lptrc    = new long[1][];
+        double[][] lvalc  = new double[1][];
+
+        /*TAG:begin-factorization*/
+        env.computesparsecholesky(0,        //Mosek chooses number of threads
+                                  1,        //Apply reordering heuristic
+                                  1.0e-14,  //Singularity tolerance
+                                  anzc, aptrc, asubc, avalc,
+                                  perm, diag,
+                                  lnzc, lptrc, lensubnval, lsubc, lvalc);
+
+        printsparse(n, perm[0], diag[0], lnzc[0], lptrc[0], lensubnval[0], lsubc[0], lvalc[0]);
+
+        /* Permuted b is stored as x. */
+        double[] x = new double[n];
+        for (int i = 0; i < n; i++) x[i] = b[perm[0][i]];
+
+        /*Compute  inv(L)*x.*/
+        env.sparsetriangularsolvedense(mosek.transpose.no, lnzc[0], lptrc[0], lsubc[0], lvalc[0], x);
+        /*Compute  inv(L^T)*x.*/
+        env.sparsetriangularsolvedense(mosek.transpose.yes, lnzc[0], lptrc[0], lsubc[0], lvalc[0], x);
+
+        System.out.print("\nSolution A x = b, x = [ ");
+        for (int i = 0; i < n; i++)
+          for (int j = 0; j < n; j++) if (perm[0][j] == i) System.out.print(x[j] + " ");
+        System.out.println("]\n");
+        /*TAG:end-factorization*/
+      }
+
+      {
+        /*TAG:begin-example2 */
+        int n                 = 3;
+        int[] anzc            = {3, 2, 1};
+        int[] asubc           = {0, 1, 2, 1, 2, 2};
+        long[] aptrc          = {0, 3, 5, };
+        double[] avalc        = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
+        /*TAG:end-example2 */
+
+        int[][] perm      = new int[1][];
+        int[][] lnzc      = new int[1][];
+        int[][] lsubc     = new int[1][];
+        long[] lensubnval = new long[1];
+        double[][] diag   = new double[1][];
+        long[][] lptrc    = new long[1][];
+        double[][] lvalc  = new double[1][];
+
+        env.computesparsecholesky(0,        //Mosek chooses number of threads
+                                  1,        //Apply reordering heuristic
+                                  1.0e-14,  //Singularity tolerance
+                                  anzc, aptrc, asubc, avalc,
+                                  perm, diag,
+                                  lnzc, lptrc, lensubnval, lsubc, lvalc);
+
+        printsparse(n, perm[0], diag[0], lnzc[0], lptrc[0], lensubnval[0], lsubc[0], lvalc[0]);
+      }
+    } catch (mosek.Exception e) {
+      System.out.println("An error was encountered");
+      System.out.println(e.getMessage());
+      throw e;
+    }
+  }
+}
