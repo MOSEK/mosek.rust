02b292589d0816bd52a6922d8b444c0949b26e11
ulfw <ulf.worsoe@mosek.com>
2026-07-27 08:15:22 +0000

    fix #1: pass libmosek and link paths correctly

--- build.rs
+++ build.rs
@@ -229,12 +229,12 @@ fn main() {
         else { getmosek(&pfname, mskvermajor, mskverminor) };
 
     println!("cargo:rustc-link-search={}",libdir);
-    println!("cargo:rustc-flags=-L {} -l {}",libdir,libname);
+    println!("cargo:rustc-link-lib={}",libname);
 
     if cfg!(target_os = "linux") {
-        println!("cargo:rustc-link-arg=-Wl,-rpath={}",libdir);
+        println!("cargo:rustc-link-arg=-Wl,-rpath=\"{}\"",libdir);
     }
     else if cfg!(target_os = "macos") {
-        println!("cargo:rustc-link-arg=-Wl,-rpath,{}",libdir);
+        println!("cargo:rustc-link-arg=-Wl,-rpath,\"{}\"",libdir);
     }
 }
